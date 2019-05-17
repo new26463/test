a95a49bc0911d4a31737ababd06338b75a3da418
new26463 <new26463@gmail.com>
2019-05-17 11:01:21 +0700

    Update keystone.js

--- keystone.js
+++ keystone.js
@@ -46,9 +46,6 @@ keystone.set('nav', {
 	users: 'users',
 });
 
-keystone.set('host', '54.179.165.32');
-keystone.set('port', 80);
-
 keystone.set('cloudinary config', 'cloudinary://333779167276662:_8jbSi9FB3sWYrfimcl8VKh34rI@keystone-demo' );
 
 keystone.start();
